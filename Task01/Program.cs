c92945ab3399a082bfc9646a7f47deba35713569
Art <artemgoryunov89@gmail.com>
2023-07-20 15:51:39 +0400

    Добавили папку Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("1ое число является квадратом 2ого");
+}
+
+else
+{
+    Console.WriteLine("1ое число не является квадратом 2ого");
+}
